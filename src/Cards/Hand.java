603caf87b4ad8ba1f112d5141bd331f1070f0834
drabchuk <denis.drabchuck@gmail.com>
2015-05-15 16:40:27 +0300

    Колода мешается + всякое дерьмо

---
+++ src/Cards/Hand.java
@@ -0,0 +1,14 @@
+package Cards;
+
+/**
+ * Created by Δενθρ on 15.05.2015.
+ */
+public class Hand {
+    public Card c1;
+    public Card c2;
+
+    Hand (Card card1, Card card2) {
+        c1 = card1;
+        c2 = card2;
+    }
+}
--- src/Players/Player.java
+++ src/Players/Player.java
@@ -1,7 +1,27 @@
 package Players;
 
+import Cards.Hand;
+
 /**
  * Created by Δενθρ on 15.05.2015.
  */
 public class Player {
+    Hand hand;
+
+    Player () {
+        hand = null;
+    }
+    Player (int brll) {
+        hand = null;
+    }
+
+    void GiveAHand(Hand h) {
+        hand = h;
+    }
+
+    Hand TakeAHand() {
+        Hand tmp = hand;
+        hand = null;
+        return tmp;
+    }
 }
